cd80129a7385399a2637eaf0f2e7884b47d24fcc
piercepatrick <piercepatrick2001@gmail.com>
2021-05-02 13:41:02 -0500

    Add UI file and class

---
+++ src/UI.js
@@ -0,0 +1,23 @@
+
+
+class UI {
+    constructor() {
+
+    }
+
+    loadHomepage() {
+
+    }
+    
+    addTaskToScreen() {
+        
+    }
+
+    removeAddTaskBtn() {
+        let addTaskBtn = document.getElementById('button-add-task')
+        addTaskBtn.style.display = 'none !important'
+    }
+
+}
+
+export { UI }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-/*
-    will have projects. to dos are apart of projects
-    will have to do objects with properties title, description, dueDate, priority, notes
-    user will be able to create new projects and choose wich project their todos go into
-
-    seperate application logic from dom related stuff
+import { UI } from './UI.js'
 
 
-    When a user first opens the app, there should be some sort of ‘default’ project to which all of their todos are put.
-*/
-
-
-    
+document.addEventListener('DOMContentLoaded', UI.loadHomepage)
